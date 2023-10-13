da6a09b240cb684ae8ab6283e957bf8327c68f64
davidteather <34144122+davidteather@users.noreply.github.com>
2023-10-12 20:39:25 -0500

    add api route to get db records

--- db/init_db.py
+++ db/init_db.py
@@ -18,19 +18,35 @@ cursor = conn.cursor()
 
 # Create table if it doesn't exist
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
-);
-""")
-
-# Populate the table (as an example)
-cursor.execute("""
-INSERT INTO users (name, email)
-VALUES ('John Doe', 'john@example.com'),
-       ('Jane Smith', 'jane@example.com')
-ON CONFLICT (email) DO NOTHING;
+CREATE TABLE IF NOT EXISTS official_satellites ( 
+    official_name VARCHAR(255) PRIMARY KEY,
+    reg_country VARCHAR(255),
+    own_country VARCHAR(255),
+    owner_name VARCHAR(255),
+    user_type VARCHAR(255),
+    purposes VARCHAR(255),
+    orbit_class VARCHAR(16), 
+    orbit_type VARCHAR(255),
+    geo_longitude VARCHAR(255),
+    perigee VARCHAR(255),
+    apogee VARCHAR(255),
+    eccentricity VARCHAR(255),
+    inclination VARCHAR(255),
+    period_min VARCHAR(255),
+    mass_launch VARCHAR(255),
+    mass_dry VARCHAR(255),
+    power_watts VARCHAR(255),
+    launch_date VARCHAR(255),
+    exp_lifetime VARCHAR(255),
+    contractor VARCHAR(255),
+    contractor_country VARCHAR(255),
+    launch_site VARCHAR(255),
+    launch_vehicle VARCHAR(255),
+    cospar_number  VARCHAR(20),
+    norad_number integer,
+    comment_note VARCHAR(255),
+    source_orbit VARCHAR(255)
+    );
 """)
 
 conn.commit()
