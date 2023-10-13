1706092bb7ebc11c248789135b876806dcdd226b
davidteather <34144122+davidteather@users.noreply.github.com>
2023-10-13 12:19:27 -0500

    add pagination and docs on /confirmed/satellites

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 import os
 
@@ -24,13 +24,43 @@ def get_db_connection():
 
 @app.route('/confirmed/satellites')
 def get_satellites():
-    """Get all satellites from the official_satellites table."""
+    """
+    Retrieve satellites from the official_satellites table with optional pagination.
+    
+    URL Parameters:
+        - limit (optional): An integer specifying the number of records to return.
+            If not provided, all records are returned.
+        - page (optional): An integer specifying the page number when using pagination.
+            Defaults to 1 if not provided. Used in conjunction with the 'limit' parameter 
+            to determine the records offset.
+
+    Example Usage:
+        - /confirmed/satellites?limit=10&page=2: Returns records 11-20
+        - /confirmed/satellites?limit=5: Returns the first 5 records
+        - /confirmed/satellites: Returns all records
+
+    Returns:
+        A JSON representation of the selected satellite records.
+    """
+    
+    # Get the optional limit and page parameters from the request URL
+    limit = request.args.get('limit', default=None, type=int)
+    page = request.args.get('page', default=1, type=int)
+    
+    # Calculate offset based on limit and page
+    offset = (page - 1) * limit if limit else 0
+
     conn = get_db_connection()
     cursor = conn.cursor()
+
+    # Modify the SQL query to use LIMIT and OFFSET for pagination
+    if limit:
+        cursor.execute("SELECT * FROM official_satellites LIMIT %s OFFSET %s", (limit, offset))
+    else:
+        cursor.execute("SELECT * FROM official_satellites")
     
-    cursor.execute("SELECT * FROM official_satellites")
     satellites = cursor.fetchall()
-    
+
     # Close the connection
     cursor.close()
     conn.close()
@@ -40,6 +70,7 @@ def get_satellites():
     satellites_as_dict = [dict(zip(columns, row)) for row in satellites]
 
     return jsonify({'satellites': satellites_as_dict})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
