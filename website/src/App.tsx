554b4f9a0a3f52f6d9eacef15ec04b7353363811
Adam Schmidt <amschmidt9@wisc.edu>
2023-10-15 13:14:39 -0500

    Trash react code to make table for demo. Attempted to implement hitting the api endpoint, ran into cors issues

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react'
 import Axios from 'axios';
+import placeholder from './assets/placeholderData.json'
 import './App.css'
-import SatelliteData from './types/Satellite';
+import {SatelliteData} from './types/Satellite';
+import SatelliteTable from './components/satelliteTable';
 
 function App() {
 
   const [satellites, setSatellites] = useState<SatelliteData>({satellites: []})
 
   useEffect(() => {
-    const getData = async () =>{
-      try{
-        const satelliteData = await Axios.get<SatelliteData>("http://localhost:8080/confirmed/satellites?limit=10");
-        setSatellites(satelliteData.data);
-      }
-      catch(error){
-        console.error("An error occurred fetching satellite data.", error);
-      }
-    }    
-    getData();
+    setSatellites(placeholder);
+    
+
+    // const getData = async () =>{
+    //   try{
+    //     const satelliteData = await Axios.get<SatelliteData>("http://localhost:8080/confirmed/satellites?limit=10");
+    //     setSatellites(satelliteData.data);
+    //   }
+    //   catch(error){
+    //     console.error("An error occurred fetching satellite data.", error);
+    //   }
+    // }    
+    // getData();
   }, []);
 
 
@@ -28,7 +33,8 @@ function App() {
         width: 800,
         paddingTop: "1rem"
       }}>
-        <h1>{JSON.stringify(satellites)}</h1>
+        <h1>Satellite Data</h1>
+        <SatelliteTable satellites={satellites.satellites}/>
 
       </div>
     </>
--- website/src/types/Satellite.ts
+++ website/src/types/Satellite.ts
@@ -1,37 +1,35 @@
-interface SatelliteData {
-  satellites: Satellite[];
+export interface SatelliteData {
+    satellites: Satellite[];
 }
 
-interface Satellite {
-  apogee: string;
-  comment_note: string;
-  contractor: string;
-  contractor_country: string;
-  cospar: string;
-  detailed_purpose: string;
-  eccentricity: string;
-  exp_lifetime: string;
-  geo_longitude: string;
-  inclination: string;
-  launch_date: string;
-  launch_site: string;
-  launch_vehicle: string;
-  mass_dry: string | null;
-  mass_launch: string;
-  norad: number;
-  official_name: string;
-  orbit_class: string;
-  orbit_type: string;
-  own_country: string;
-  owner_name: string;
-  perigee: string;
-  period_min: string;
-  power_watts: string | null;
-  purposes: string;
-  reg_country: string;
-  source_orbit: string | null;
-  source_satellite: string[] | null;
-  user_type: string;
+export interface Satellite {
+    apogee:             string;
+    comment_note:       string;
+    contractor:         string;
+    contractor_country: string;
+    cospar:             string;
+    detailed_purpose:   string;
+    eccentricity:       string;
+    exp_lifetime:       string;
+    geo_longitude:      string;
+    inclination:        string;
+    launch_date:        string;
+    launch_site:        string;
+    launch_vehicle:     string;
+    mass_dry:           null;
+    mass_launch:        string;
+    norad:              number;
+    official_name:      string;
+    orbit_class:        string;
+    orbit_type:         string;
+    own_country:        string;
+    owner_name:         string;
+    perigee:            string;
+    period_min:         string;
+    power_watts:        null;
+    purposes:           string;
+    reg_country:        string;
+    source_orbit:       null;
+    source_satellite:   Array<null | string>;
+    user_type:          string;
 }
-
-export default SatelliteData;
