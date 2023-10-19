a249c46221e2d06f63f2b54c97acc78a7e972267
AdamSchmidty <71237254+AdamSchmidty@users.noreply.github.com>
2023-10-19 12:30:33 -0500

    Adam prettier table (#4)
    * Work on shadcn ui table
    
    * Displaying satellite data in table. Fixed null values displaying in
    array output for source_satellite. TODO: better handle null values (each
    key + v pair can be potentially null) and fix source satellite display
    being hella ugly
    
    * Added ability to toggle columns. Moved column definitions + pagination
    size + url + visibility defaults into a new constants.ts.
    
    ---------
    
    Co-authored-by: Saq <adamschmidt2023@gmail.com>
    Co-authored-by: Adam Schmidt <amschmidt9@wisc.edu>

---
+++ website/src/components/SatelliteTable/columns.tsx
@@ -0,0 +1,6 @@
+import { ColumnDef } from "@tanstack/react-table"
+import { Satellite } from "../../types/Satellite"
+import { columnDefinitions } from "@/Constants/constants";
+ 
+
+export const satelliteColumns: ColumnDef<Satellite>[] = columnDefinitions;
---
+++ website/src/components/SatelliteTable/data-table.tsx
@@ -0,0 +1,133 @@
+"use client"
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  VisibilityState,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {Button} from "@/components/ui/button"
+
+import {useState} from 'react'
+import { columnVisibilityDefaults } from "@/Constants/constants"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(columnVisibilityDefaults)
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      columnVisibility,
+    },
+  })
+
+  return (
+    <div>
+         <div className="flex items-center py-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Filter Columns
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter(
+                (column) => column.getCanHide()
+              )
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                )
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
