b47e3efa62e3bda023c3e3a7f71382d0e04832f0
wrldbf <98567119+wrldbf@users.noreply.github.com>
2023-10-22 11:31:09 -0500

    update 'PUT' operation & rename

--- api/routes/edit.py
+++ api/routes/edit.py
@@ -4,59 +4,60 @@ from utils.helpers import get_db_connection
 # Create a Blueprint for this subpath
 edit_subpath = Blueprint('edit', __name__)
 
-@edit_subpath.route('/create', methods=['POST'])
-def create():
+@edit_subpath.route('/int:satellite_id', methods=['PUT'])
+def update(satellite_id):
     """
-    Executes a creat function , such as an INSERT INTO statement on the edit table.
+     Update a record in the official_satellites table.
+
+    Parameters:
+        - satellite_id (int): The unique identifier of the satellite to update.
+
+    Request Data:
+        - data (str): The updated data for the satellite.
+        - source (str): The updated source for the satellite.
+
+    Returns:
+        A JSON response indicating the success or failure of the update.
+
+    Example Usage:
+        PUT /edit/<valid_satellite_id>
+        Body: data=new_data & source=new_source
+
+    Returns:
+        A JSON representation of the edited satellite records
     """
-    if request.method == 'POST':
-        data = request.form['data']
-        source = request.form['source']
+    data = request.form['data']
+    source = request.form['source']
 
-        conn = get_db_connection()
-        cursor = conn.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor()
 
-        # Insert a new record into the official_satellites table
-        insert_query = "INSERT INTO official_satellites (data, source) VALUES (%s, %s)"
-        cursor.execute(insert_query, (data, source))
-        conn.commit()
+    # The SQL query to update the official_satellites table
+    update_query = "UPDATE official_satellites SET data = %s, source = %s WHERE id = %s"
+    cursor.execute(update_query, (data, source, satellite_id))
 
-        cursor.close()
-        conn.close()
+    cursor.execute("SELECT data, source FROM official_satellites WHERE id = %s", (satellite_id,))
+    edited_data, edited_source = cursor.fetchone()
 
-
-@edit_subpath.route('/update/<int:id>', methods=['PUT'])
-def update(id):
+    create_log_table_query = """
+        CREATE TABLE IF NOT EXISTS log (
+            official_name VARCHAR(255) PRIMARY KEY,
+            action VARCHAR(10),
+            log_data VARCHAR(255),
+            source text
+        )
     """
-    Executes a update function , such as an UPDATE statement on the edit table.
-    """
-    if request.method == 'PUT':
-        data = request.form['data']
-        source = request.form['source']
+    cursor.execute(create_log_table_query)
 
-        conn = get_db_connection()
-        cursor = conn.cursor()
+    # The SQL query to insert a log into the log table
+    log_query = "INSERT INTO log (satellite_id, action, data, source) VALUES (%s, 'edit', %s, %s)"
+    cursor.execute(log_query, (id, data, source))
 
-        update_query = "UPDATE official_satellites SET data = %s, source = %s WHERE id = %s"
-        cursor.execute(update_query, (data, source, id))
-        conn.commit()
+    conn.commit()
 
-        cursor.close()
-        conn.close()
+    # Close the connection
+    cursor.close()
+    conn.close()
 
+    return jsonify({'message': 'Edit successful', 'data': edited_data, 'source': edited_source})
 
-@edit_subpath.route('/delete/<int:id>', methods=['DELETE'])
-def delete(id):
-    """
-    Executes a delete function , such as an DELETE statement on the edit table.
-    """
-    if request.method == 'DELETE':
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        delete_query = "DELETE FROM official_satellites WHERE id = %s"
-        cursor.execute(delete_query, (id,))
-        conn.commit()
-
-        cursor.close()
-        conn.close()
