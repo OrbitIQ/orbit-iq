f4e1a3f84799dcb6c2c8ab990b798f573d262351
Gugu0099 <julati@wisc.edu>
2023-10-29 14:12:54 -0500

    added edit button that route to the update page & simple pagination

--- website/src/pages/DataPage.tsx
+++ website/src/pages/DataPage.tsx
@@ -1,23 +1,30 @@
 import SatelliteTable from "../components/SatelliteTable/page";
+import { Button } from "../components/ui/button";
+import { Link } from "react-router-dom";
 
 function DataPage() {
-    return (
-        <>
-            <div
-                style={{
-                    margin: "auto",
-                    width: 1600,
-                    paddingTop: "2.3rem",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <h1>Satellite Data</h1>
-                <SatelliteTable />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        style={{
+          margin: "auto",
+          width: 1600,
+          paddingTop: "2.3rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1>Satellite Data</h1>
+        <SatelliteTable />
+        <Link to="/updates">
+          <Button variant="outline" size="icon">
+            Edit
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default DataPage;
