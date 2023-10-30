799160b9e1defb5fd07c8ed2a691efa3b3eddb63
nori210 <georgia1024@outlook.com>
2023-10-30 14:23:34 -0500

    update helpers.py

--- api/utils/helpers.py
+++ api/utils/helpers.py
@@ -1,5 +1,7 @@
 import psycopg2
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # Database connection configurations using os.environ
 DB_HOST = os.environ.get('DB_HOST')
@@ -7,6 +9,10 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('POSTGRES_DB')
 DB_USER = os.environ.get('POSTGRES_USER')
 DB_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 if any(v is None for v in [DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD]):
     raise Exception("One or more environment variables are missing.")
