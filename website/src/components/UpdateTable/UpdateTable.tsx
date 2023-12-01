bd2e4e1b8e002353d51c272f368249bf1d9aff1e
nori210 <georgia1024@outlook.com>
2023-12-01 15:45:00 -0600

    save the confirmation dialog flow

--- website/src/components/UpdateTable/UpdateTable.tsx
+++ website/src/components/UpdateTable/UpdateTable.tsx
@@ -53,20 +53,41 @@ export default function UpdateTable() {
       }
     };
     getData();
-  }, []);
+  }, [])
 
-  const handleApprove:HandleChangeFunction = async (rowId: number) => {
+  const handleApprove: HandleChangeFunction = async (rowId: number) => {
     try {
-      const response = await Axios.put(
-        `${proposedChangeURL}/approve/${rowId}`
-      );
-      if (response.status === 200) {
-        console.log("Approved:", response.data.id);
+      // Update the row to mark as 'approved' before asking for confirmation
+      setUpdate(prevState => ({
+        ...prevState,
+        proposed_changes: prevState.proposed_changes.map(item => {
+          if (item.id === rowId) {
+            return { ...item, is_approved: "approved" };
+          }
+          return item;
+        })
+      }));
+
+      if (window.confirm("Are you sure you want to approve this record?")) {
+        const response = await Axios.put(`${proposedChangeURL}/approve/${rowId}`);
+        if (response.status === 200) {
+          console.log("Approved:", response.data.id);
+          // Remove the row from the displayed data
+          setUpdate(prevState => ({
+            ...prevState,
+            proposed_changes: prevState.proposed_changes.filter(item => item.id !== rowId)
+          }));
+  
+          // Alert the user about successful approval
+          alert("Approval confirmed! The row has been moved to the verified data page.");
+        }
+      } else {
+        // If not confirmed, revert the is_approved status
         setUpdate(prevState => ({
           ...prevState,
           proposed_changes: prevState.proposed_changes.map(item => {
             if (item.id === rowId) {
-              return { ...item, is_approved: "approved" };
+              return { ...item, is_approved: "pending" }; 
             }
             return item;
           })
@@ -74,32 +95,34 @@ export default function UpdateTable() {
       }
     } catch (error) {
       console.error("Error approving:", error);
-      // TODO: do more interaction
     }
   };
+  
+  
 
-  const handleDeny:HandleChangeFunction = async (rowId: number) => {
+  const handleDeny: HandleChangeFunction = async (rowId: number) => {
     try {
-      const response = await Axios.put(
-        `${proposedChangeURL}/deny/${rowId}`
-      );
-      if (response.status === 200) {
-        console.log("Denied:", response.data.id);
-        setUpdate(prevState => ({
-          ...prevState,
-          proposed_changes: prevState.proposed_changes.map(item => {
-            if (item.id === rowId) {
-              return { ...item, is_approved: "denied" };
-            }
-            return item;
-          })
-        }));
+      if (window.confirm("Are you sure you want to deny this item?")) {
+        const response = await Axios.put(`${proposedChangeURL}/deny/${rowId}`);
+        if (response.status === 200) {
+          console.log("Denied:", response.data.id);
+          setUpdate(prevState => ({
+            ...prevState,
+            proposed_changes: prevState.proposed_changes.map(item => {
+              if (item.id === rowId) {
+                // Update the is_approved status and disable actions
+                return { ...item, is_approved: "denied", actionsDisabled: true };
+              }
+              return item;
+            })
+          }));
+        }
       }
     } catch (error) {
       console.error("Error denying:", error);
-      // TODO: do more interaction
     }
   };
+  
 
   return (
     <div className="container mx-auto py-10">
--- website/src/components/UpdateTable/columns.tsx
+++ website/src/components/UpdateTable/columns.tsx
@@ -11,34 +11,42 @@ type UpdateColumnsProps = {
 };
 
 const UpdateColumns = ({ handleApprove, handleDeny }: UpdateColumnsProps): ColumnDef<Update>[] => {
-    return updateColumns.map(column => {
-      if (column.id === 'approve' || column.id === 'deny') {
-        return {
-          ...column,
-          accessorFn: row => row as Update, // this is just for type compacitity
-          cell: ({ row }: { row: Row<Update> }) => (
-            <Button 
-              variant={column.id === 'approve' ? 'success' : 'destructive'}
-              onClick={() => {
-                const rowId = Number(row.original.id); // Convert to number
-                return column.id === 'approve' ? handleApprove(rowId) : handleDeny(rowId);
-              }}
-            >
-              {column.id.charAt(0).toUpperCase() + column.id.slice(1)}
-            </Button>
-          )
-        };
-      }
+  return updateColumns.map(column => {
+    if (column.id === 'approve' || column.id === 'deny') {
       return {
-          ...column,
-          accessorFn: row => {
-            if (typeof column.accessorKey === 'string') { // Check if accessorKey is a string
-              return row[column.accessorKey];
-            }
-            return ''; // Return a default value if accessorKey is undefined
-          },
+        ...column,
+        accessorFn: row => row as Update,
+        cell: ({ row }: { row: Row<Update> }) => {
+          const isApproved = row.original.is_approved === 'approved';
+          const isDenied = row.original.is_approved === 'denied';
+
+          return (
+            <div>
+              <Button 
+                variant={column.id === 'approve' ? 'success' : 'destructive'}
+                onClick={() => {
+                  const rowId = Number(row.original.id);
+                  return column.id === 'approve' ? handleApprove(rowId) : handleDeny(rowId);
+                }}
+                disabled={isApproved || isDenied} // Disable button if the row is approved or denied
+              >
+                {column.id.charAt(0).toUpperCase() + column.id.slice(1)}
+              </Button>
+            </div>
+          );
+        }
       };
-    });
-  };
+    }
+    return {
+      ...column,
+      accessorFn: row => {
+        if (typeof column.accessorKey === 'string') {
+          return row[column.accessorKey];
+        }
+        return '';
+      },
+    };
+  });
+};
 
 export default UpdateColumns;
