87871d751d1d456737c9c522984b8c2c6fe3c1ac
davidteather <34144122+davidteather@users.noreply.github.com>
2023-12-06 20:08:17 -0600

    add edit & delete

--- website/src/components/Authentication/UserManagement.tsx
+++ website/src/components/Authentication/UserManagement.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import api from '@/services/AxiosInterceptor'; // Adjust the import path as needed
+import { useState, useEffect } from 'react';
+import api from '@/services/AxiosInterceptor';
 import RegistrationModal from './UserRegistrationModal';
+import EditUserModal from './UserEditModal';
 
 type User = {
     username: string;
@@ -11,7 +12,28 @@ type User = {
 const UserManagement = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingUser, setEditingUser] = useState<User>();
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
+  const handleEditUser = (user: User) => {
+    setEditingUser(user);
+    setIsEditModalOpen(true);
+  };
+
+  const handleUpdateUser = (updatedUser: User) => {
+    setIsEditModalOpen(false);
+    api.post(`/auth/edit`, {
+      username: updatedUser.username,
+      name: updatedUser.name,
+      is_admin: updatedUser.is_admin
+    }).then(() => {
+      window.location.reload();
+    })
+  };
+
+  const handleCloseEditModal = () => {
+    setIsEditModalOpen(false);
+  };
 
   useEffect(() => {
     api.get('/auth/list')
@@ -30,7 +52,27 @@ const UserManagement = () => {
     setIsModalOpen(false);
   };
 
+  const handleDeleteUser = (username: string) => {
+    if (!window.confirm(`Are you sure you want to delete the user "${username}"?`)) {
+      return;
+    }
+    api.post(`/auth/delete`, {
+      username: username
+    }).then(() => {
+      window.location.reload();
+    })
+  };
+
   return (
+    <>
+    {isEditModalOpen && editingUser && (
+      <EditUserModal
+        isOpen={isEditModalOpen}
+        onClose={handleCloseEditModal}
+        user={editingUser}
+        onEdit={handleUpdateUser}
+      />
+    )}
     <div className="p-6 bg-white shadow-md rounded-lg">
       <h1 className="text-2xl font-semibold mb-4">User Management</h1>
 
@@ -75,8 +117,18 @@ const UserManagement = () => {
                   {user.is_admin ? 'Yes' : 'No'}
                 </td>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                  <button className="text-blue-600 hover:text-blue-900">Edit</button>
-                  <button className="text-red-600 hover:text-red-900 ml-4">Delete</button>
+                <button 
+                    className="text-blue-600 hover:text-blue-900"
+                    onClick={() => handleEditUser(user)}
+                  >
+                    Edit
+                  </button>
+                  <button 
+                    className="text-red-600 hover:text-red-900 ml-4"
+                    onClick={() => handleDeleteUser(user.username)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
@@ -85,6 +137,7 @@ const UserManagement = () => {
       </div>
 
     </div>
+    </>
   );
 };
 
